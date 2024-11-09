27c2185a72dd7e626f8933ec0d4ae516291ae943
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-09 05:22:37 +0000

    Add `NimBLEError` error codes

--- Sources/NimBLE/Error.swift
+++ Sources/NimBLE/Error.swift
@@ -5,6 +5,8 @@
 //  Created by Alsey Coleman Miller on 11/6/24.
 //
 
+import CNimBLE
+
 /// NimBLE error codes
 public struct NimBLEError: Error, RawRepresentable, Equatable, Hashable, Sendable {
     
@@ -14,6 +16,40 @@ public struct NimBLEError: Error, RawRepresentable, Equatable, Hashable, Sendabl
         self.rawValue = rawValue
     }
 }
+
+// MARK: - CustomStringConvertible
+
+extension NimBLEError: CustomStringConvertible {
+    
+    public var description: String {
+        rawValue.description
+    }
+}
+
+// MARK: - Error Codes
+
+public extension NimBLEError {
+    
+    /// Operation failed and should be retried later.
+    static var retryAgain: NimBLEError { NimBLEError(rawValue: BLE_HS_EAGAIN) }
+    
+    /// Operation already in progress.
+    static var alreadyInProgress: NimBLEError { NimBLEError(rawValue: BLE_HS_EALREADY) }
+    
+    /// Invalid parameter.
+    static var invalidParameter: NimBLEError { NimBLEError(rawValue: BLE_HS_EINVAL) }
+    
+    /// Message too long.
+    static var messageSize: NimBLEError { NimBLEError(rawValue: BLE_HS_EMSGSIZE) }
+
+    /// No such entry.
+    static var noEntry: NimBLEError { NimBLEError(rawValue: BLE_HS_ENOENT) }
+    
+    /// Out of memory.
+    static var outOfMemory: NimBLEError { NimBLEError(rawValue: BLE_HS_ENOMEM) }
+}
+
+// MARK: - Extensions
 
 internal extension Int32 {
     
