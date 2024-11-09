5691018ddcde46563df189136626e1723c459c36
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-09 17:52:27 +0000

    Update demo

--- Sources/NimBLEDemo/NimBLEDemo.swift
+++ Sources/NimBLEDemo/NimBLEDemo.swift
@@ -6,16 +6,29 @@
 //
 
 import Bluetooth
+import BluetoothGATT
 import NimBLE
 
 @main
 struct NimBLEDemo {
     
     static func main() throws {
-        var bluetooth = NimBLE()
-        let address = try bluetooth.address()
+        let bluetooth = NimBLE()
+
+        // get address
+        let hostController = bluetooth.hostController
+        let address = try hostController.address()
         print("Bluetooth Address:", address)
         
+        let server = bluetooth.server
+        let service = GATTAttribute.Service(
+            uuid: .bit16(0x180A),
+            primary: true,
+            characteristics: []
+        )
+        try server.add(services: [service])
+        server.dump()
+
         // Run event loop
         bluetooth.run()
     }
